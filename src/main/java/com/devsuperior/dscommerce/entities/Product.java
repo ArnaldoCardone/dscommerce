ead3e6dc5104874c0c9c75aedb9987a089490ea7
ArnaldoCardone <arnaldo.cardone@gmail.com>
2024-11-07 10:41:54 -0300

    Implementação do OrderItem, relacionamento 1 para muitos tabela de relacionamento

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -22,7 +23,10 @@ public class Product {
     @JoinTable(name = "tb_product_category",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    Set<Category> categories = new HashSet<>();
+    private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){}
 
@@ -76,5 +80,12 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+    public List<Order> getOrders() {
+        return items.stream().map(OrderItem::getOrder).toList();
     }
 }
