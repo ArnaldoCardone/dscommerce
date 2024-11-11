7b77e437210360bc937af79ff6961471edab1832
ArnaldoCardone <arnaldo.cardone@gmail.com>
2024-11-11 18:23:22 -0300

    Exclusão do produto - DELETE

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -46,8 +46,13 @@ public class ProductService {
         CopyDtoToEntity(dto,entity);
         entity = repository.save(entity);
         return new ProductDTO(entity);
-
     }
+
+    @Transactional
+    public void delete(Long id){
+         repository.deleteById(id);;
+    }
+
     private void CopyDtoToEntity(ProductDTO dto, Product entity){
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
