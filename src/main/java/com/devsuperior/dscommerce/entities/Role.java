b7b3db00b73ff629f73aa174f71a0bc4a138b819
ArnaldoCardone <arnaldo.cardone@gmail.com>
2025-02-03 18:46:29 -0300

    Implementação do UserDetails e Authorizathion Server

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Table(name = "tb_role")
-public class Role implements GrantedAuthority {
+public class Role implements  GrantedAuthority  {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
