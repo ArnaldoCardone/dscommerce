9cb9b41b915c0ae54b0b8c7fa180f1c0551739e8
ArnaldoCardone <arnaldo.cardone@gmail.com>
2025-02-04 17:09:51 -0300

    Implementação do ProductMinDTO para listagem

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductMinDTO.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dscommerce.dto;
+
+
+import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+public class ProductMinDTO {
+
+    private long id;
+    private String name;
+    private double price;
+    private String imgUrl;
+
+    public ProductMinDTO(){}
+
+    public ProductMinDTO(long id, String name, double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProductMinDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.price = entity.getPrice();
+        this.imgUrl = entity.getImgUrl();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
